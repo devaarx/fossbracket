206f0109273bad8fee4fa6f9cd36f909f581e510
iamdebadipti <iamdebadipti@gmail.com>
2020-03-08 23:55:04 +0530

    removed default util function & added typography config file

---
+++ src/utils/typography.js
@@ -0,0 +1,15 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.78,
+  headerFontFamily: ['Open Sans', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Merriweather', 'Georgia', 'serif'],
+  headerGray: 10,
+  bodyGray: 10,
+  headerWeight: 700,
+  bodyWeight: 300,
+  boldWeight: 700,
+});
+
+export default typography;
