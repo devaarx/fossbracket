a67ab731cf0242ea1436187afd84f2f4ef02c29d
iamdebadipti <iamdebadipti@gmail.com>
2020-04-04 22:50:37 +0530

    removed tags from articles

--- src/components/Pages/Feed.js
+++ src/components/Pages/Feed.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import moment from 'moment';
 import { Link } from 'gatsby';
-import Tags from '../Post/Tags';
 
-const Feed = ({ edges }) => (
-  <div>
-    {edges.map(edge => (
-      <div key={edge.node.fields.slug}>
-        <div>
-          <time dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-            {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
-          </time>
-          <span>
-            <Link to={edge.node.fields.categorySlug}>{edge.node.frontmatter.category}</Link>
-          </span>
-        </div>
-        <h2>
-          <Link to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
-        </h2>
-        <p>{edge.node.frontmatter.description}</p>
-        <Link to={edge.node.fields.slug}>Read</Link>
-        {edge.node.frontmatter.tags && edge.node.fields.tagSlugs && (
-          <Tags tags={edge.node.frontmatter.tags} tagSlugs={edge.node.fields.tagSlugs} />
-        )}
-      </div>
-    ))}
-  </div>
-);
+function Feed({ edges }) {
+  return (
+    <div className="feed">
+      {edges.map((edge) => (
+        <article key={edge.node.fields.slug} className="feed_article">
+          <div className="feed_article_detail">
+            <time dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
+              {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
+            </time>
+            <b>&#9679;</b>
+            <span>
+              <Link to={edge.node.fields.categorySlug}>{edge.node.frontmatter.category}</Link>
+            </span>
+          </div>
+          <h2 className="feed_article_heading">
+            <Link to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+          </h2>
+          <p className="feed_article_description">{edge.node.frontmatter.description}</p>
+          <div className="feed_article_bottom">
+            <Link to={edge.node.fields.slug}>Read more...</Link>
+            {/* {edge.node.frontmatter.tags && edge.node.fields.tagSlugs && (
+              <Tags tags={edge.node.frontmatter.tags} tagSlugs={edge.node.fields.tagSlugs} />
+            )} */}
+          </div>
+        </article>
+      ))}
+    </div>
+  );
+}
 
 export default Feed;
