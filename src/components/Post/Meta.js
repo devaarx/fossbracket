30394921825c10ff5102952e8156e3bf29054e9d
iamdebadipti <iamdebadipti@gmail.com>
2020-04-05 15:05:16 +0530

    article styles added

--- src/components/Post/Meta.js
+++ src/components/Post/Meta.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 
 const Meta = ({ date }) => (
-  <div>
+  <div className="article_meta">
     <p>Published {moment(date).format('D MMM YYYY')}</p>
   </div>
 );
--- src/components/Post/Tags.js
+++ src/components/Post/Tags.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Tags = ({ tags, tagSlugs }) => (
-  <div>
+  <div className="article_tags">
     {tagSlugs &&
       tagSlugs.map((slug, i) => (
         <Link key={tags[i]} to={slug}>
