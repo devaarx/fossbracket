adbcc9bc3b9a4ae65f10d74e13dab4238fb233f3
iamdebadipti <iamdebadipti@gmail.com>
2020-08-05 12:20:42 +0530

    added fix for mini-css-extract-plugin order issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -100,3 +100,17 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     });
   }
 };
+
+// FIX: warn chunk styles [mini-css-extract-plugin] Conflicting order
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig();
+    const miniCssExtractPlugin = config.plugins.find(
+      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin'
+    );
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true;
+    }
+    actions.replaceWebpackConfig(config);
+  }
+};
